30d19fc4bf4befa3aeeff2b49d0ca4f415dcf3fa
nilenuru <41341054+nilenuru@users.noreply.github.com>
2019-02-23 17:27:40 +0300

    Форма

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var search=document.querySelector(".search-button");
 var modal=document.querySelector(".modal-search");
-var date=modal.querySelector("[name=date-of-arrival]")
+var date=modal.querySelector("[name=date]")
+var form=modal.querySelector("form");
+var name=modal.querySelector("[name=people-name]");
 
 
 
@@ -8,4 +10,11 @@ search.addEventListener("click", function (evt) {
 evt.preventDefault();
 modal.classList.toggle("modal-show");
 date.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (date.value||name.value){
+evt.preventDefault();
+console.log("Нужно ввести логин и пароль");
+  }
 });
