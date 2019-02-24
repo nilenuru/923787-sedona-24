20b23957f0fc8155476d6e3f495ba0c00964f020
nilenuru <41341054+nilenuru@users.noreply.github.com>
2019-02-24 08:59:21 +0300

    мучаю яву

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 var search = document.querySelector(".search-button");
 var modal = document.querySelector(".modal-search");
-var date = modal.querySelector("[name=date]")
+var date = modal.querySelector("[name=date-name]")
 var name = modal.querySelector("[name=people-name]");
 var form = modal.querySelector("form");
 
 search.addEventListener("click", function(evt) {
   evt.preventDefault();
-  modal.classList.toggle("modal-show");
+  if (modal.classList.contains("modal-on")){
+      modal.classList.add("modal-off");
+      modal.classList.remove("modal-on");
+      modal.classList.remove("modal-error");
+  } else {
+    modal.classList.add("modal-on");
+    modal.classList.remove("modal-off");
+  };
+
   date.focus();
 });
 
 form.addEventListener("submit", function(evt) {
   if (!date.value || !name.value) {
     evt.preventDefault();
+    modal.classList.remove("modal-on");
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.toggle("modal-error");
   }
 });
