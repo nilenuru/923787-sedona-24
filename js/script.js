bee9d2d669620334338fef946eaf4f68ce40004b
nilenuru <41341054+nilenuru@users.noreply.github.com>
2019-02-25 12:52:43 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var search = document.querySelector(".search-button");
 var modal = document.querySelector(".modal-search");
-var date = modal.querySelector("[name=date-name]")
-var name = modal.querySelector("[name=people-name]");
+var arrival = modal.querySelector("[name=arrival]");
+var departure = modal.querySelector("[name=departure]");
+var adult = modal.querySelector("[name=people-adult]");
+var childern = modal.querySelector("[name=people-childern]");
 var form = modal.querySelector("form");
 
 search.addEventListener("click", function(evt) {
@@ -13,16 +15,25 @@ search.addEventListener("click", function(evt) {
   } else {
     modal.classList.add("modal-on");
     modal.classList.remove("modal-off");
-  };
-
-  date.focus();
+  }
+  arrival.focus();
 });
 
 form.addEventListener("submit", function(evt) {
-  if (!date.value || !name.value) {
+  if (!arrival.value ||!departure.value|| !adult.value || !childern.value) {
     evt.preventDefault();
     modal.classList.remove("modal-on");
     modal.offsetWidth = modal.offsetWidth;
     modal.classList.toggle("modal-error");
+  }
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (modal.classList.contains("modal-on")) {
+      modal.classList.remove("modal-on");
+      modal.classList.add("modal-off");
+      modal.classList.add("search-form-error");
+    }
   }
 });
