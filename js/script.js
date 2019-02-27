5a1e4b8015d949ce78ada7e29a528430072bb3dc
Alexandr <zagoraevav@gmail.com>
2019-02-27 14:01:06 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -6,23 +6,25 @@ var adult = modal.querySelector("[name=people-adult]");
 var childern = modal.querySelector("[name=people-childern]");
 var form = modal.querySelector("form");
 
+modal.classList.add("modal-off");
+
 search.addEventListener("click", function(evt) {
   evt.preventDefault();
-  if (modal.classList.contains("modal-on")){
-      modal.classList.add("modal-off");
-      modal.classList.remove("modal-on");
-      modal.classList.remove("modal-error");
-  } else {
+  if (modal.classList.contains("modal-off")) {
     modal.classList.add("modal-on");
     modal.classList.remove("modal-off");
+    modal.classList.remove("modal-error");
+  } else {
+    modal.classList.add("modal-off");
+    modal.classList.remove("modal-on");
   }
   arrival.focus();
 });
 
 form.addEventListener("submit", function(evt) {
-  if (!arrival.value ||!departure.value|| !adult.value || !childern.value) {
+  if (!arrival.value || !departure.value || !adult.value || !childern.value) {
     evt.preventDefault();
-    modal.classList.remove("modal-on");
+    modal.classList.remove("modal-off");
     modal.offsetWidth = modal.offsetWidth;
     modal.classList.toggle("modal-error");
   }
